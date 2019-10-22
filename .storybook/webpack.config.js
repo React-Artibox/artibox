7a0a0a09696e8becdf60142fc6ec21acc2185cc7
Jay Chen <gcobs135102@gmail.com>
2019-10-22 17:37:39 +0800

    chore(storybook): setup environment

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const ROOT_PATH = path.resolve(__dirname, '..');
+const PACKAGES_PATH = path.resolve(ROOT_PATH, 'packages');
+const STORIES_PATH = path.resolve(ROOT_PATH, 'stories');
+
+const packages = ['slate-common'];
+const packagesAlias = packages.reduce((acc, package) => {
+  acc[`@artibox/${package}`] = path.resolve(PACKAGES_PATH, package, 'src');
+  return acc;
+}, {});
+const alias = { ...packagesAlias };
+
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    include: [PACKAGES_PATH, STORIES_PATH],
+    use: [
+      {
+        loader: require.resolve('awesome-typescript-loader'),
+        options: {
+          configFileName: './.storybook/tsconfig.json'
+        }
+      },
+      {
+        loader: require.resolve('react-docgen-typescript-loader')
+      }
+    ]
+  });
+
+  config.resolve.extensions.push('.ts', '.tsx');
+  config.resolve.alias = alias;
+
+  return config;
+};
---
+++ stories/index.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default { title: 'Artibox' };
+
+export const test = () => <div>test</div>;
