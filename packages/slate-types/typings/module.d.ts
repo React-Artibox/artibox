e3f5dc381754f44ed868d711e41a0e4c795dea0f
Jay Chen <gcobs135102@gmail.com>
2019-10-23 17:27:32 +0800

    refactor(slate-types): add generic queries and commands

--- packages/slate-types/typings/module.d.ts
+++ packages/slate-types/typings/module.d.ts
@@ -1,9 +1,22 @@
 import { Plugin, MarkPlugin } from './plugin';
 import { Renderer, MarkRenderer } from './renderer';
 
-export interface Module<P extends Plugin, R extends Renderer> {
-  plugins: P | P[];
+export interface Module<
+  T extends string,
+  Q extends string,
+  C extends string,
+  P extends Plugin<Q, C>,
+  R extends Renderer
+> {
+  type: T;
+  plugin: P;
   renderer: R;
 }
 
-export type MarkModule = Module<MarkPlugin, MarkRenderer>;
+export type MarkModule<T extends string, Q extends string, C extends string> = Module<
+  T,
+  Q,
+  C,
+  MarkPlugin<Q, C>,
+  MarkRenderer
+>;
--- packages/slate-types/typings/plugin.d.ts
+++ packages/slate-types/typings/plugin.d.ts
@@ -1,10 +1,20 @@
+import { QueryFunc, CommandFunc } from 'slate';
 import { Plugin as SlateReactPlugin } from 'slate-react';
 import { RenderMethodNames } from './renderer';
 
-export type Plugin = Omit<SlateReactPlugin, RenderMethodNames>;
+export type CommonPlugin = Omit<SlateReactPlugin, RenderMethodNames>;
 
-export type HelperPlugin<K extends keyof Plugin> = {
-  [key in K]: Exclude<Plugin[key], undefined>;
+export interface Plugin<Q extends string, C extends string> extends CommonPlugin {
+  queries: {
+    [k in Q]: QueryFunc;
+  };
+  commands: {
+    [k in C]: CommandFunc;
+  };
+}
+
+export type HelperPlugin<Q extends string, C extends string, K extends keyof Plugin<Q, C>> = {
+  [key in K]: Exclude<Plugin<Q, C>[key], undefined>;
 };
 
-export type MarkPlugin = Plugin;
+export type MarkPlugin<Q extends string, C extends string> = Plugin<Q, C>;
