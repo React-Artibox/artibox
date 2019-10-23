ab667f4f34c744b70b2157befe90a590c5d0e7bd
Jay Chen <gcobs135102@gmail.com>
2019-10-23 14:27:09 +0800

    fix(slate-plugin-bold): export constants on entry

--- packages/slate-plugin-bold/src/index.ts
+++ packages/slate-plugin-bold/src/index.ts
@@ -1,2 +1,9 @@
-export { BOLD_MARK_TYPE } from './constants';
+export {
+  BOLD_TYPE,
+  BOLD_COMPONENT,
+  BOLD_QUERY_HAS,
+  BOLD_COMMAND_ADD,
+  BOLD_COMMAND_REMOVE,
+  BOLD_COMMAND_TOGGLE
+} from './constants';
 export { BoldPlugin } from './plugin';
