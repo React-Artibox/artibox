ebd3661a3e80b1f0ee294dd52563fe37de3644bc
Jay Chen <gcobs135102@gmail.com>
2019-11-01 10:51:07 +0800

    refactor(slate-core): getQuery and getCommand
    let QueryFunc and CommandFunc be acceptable.

--- packages/slate-core/src/command.ts
+++ packages/slate-core/src/command.ts
@@ -4,12 +4,10 @@ export function hasCommand(editor: Editor, commandName: string) {
   return commandName in editor;
 }
 
-export function getCommand<F extends (...args: any[]) => Editor>(editor: Editor, commandName: string) {
-  if (!hasCommand(editor, commandName)) {
-    throw new Error(`Command ${commandName} is not registered.`);
+export function getCommand<F extends (...args: any[]) => Editor>(editor: Editor, command: string | CommandFunc) {
+  if (typeof command === 'string' && !hasCommand(editor, command)) {
+    throw new Error(`Command ${command} is not registered.`);
   }
-
-  const command: CommandFunc = (editor as any)[commandName];
 
   return (...args: Parameters<F>): Editor => editor.command(command, ...args);
 }
--- packages/slate-core/src/query.ts
+++ packages/slate-core/src/query.ts
@@ -4,12 +4,10 @@ export function hasQuery(editor: Editor, queryName: string) {
   return queryName in editor;
 }
 
-export function getQuery<F extends (...args: any[]) => any>(editor: Editor, queryName: string) {
-  if (!hasQuery(editor, queryName)) {
-    throw new Error(`Query ${queryName} is not registered.`);
+export function getQuery<F extends (...args: any[]) => any>(editor: Editor, query: string | QueryFunc) {
+  if (typeof query === 'string' && !hasQuery(editor, query)) {
+    throw new Error(`Query ${query} is not registered.`);
   }
-
-  const query: QueryFunc = (editor as any)[queryName];
 
   return (...args: Parameters<F>): ReturnType<F> => editor.query(query, ...args);
 }
