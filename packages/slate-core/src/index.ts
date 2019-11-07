9fb9b2d4301e6ace5174ba46e3c147a8671e38e3
Jay Chen <gcobs135102@gmail.com>
2019-11-07 12:22:44 +0800

    feat(slate-core): add type util

--- packages/slate-core/src/index.ts
+++ packages/slate-core/src/index.ts
@@ -1,4 +1,5 @@
 export { PARAGRAPH_TYPE } from './paragraph.constants';
+export { PickPluginAndRequired } from './typings.utils';
 
 export { Query, hasQuery, getQuery } from './query';
 export { Command, hasCommand, getCommand } from './command';
--- packages/slate-core/src/typings.utils.ts
+++ packages/slate-core/src/typings.utils.ts
@@ -1,4 +1,5 @@
 import { Editor } from 'slate';
+import { Plugin } from 'slate-react';
 
 /**
  * @example
@@ -17,3 +18,12 @@ export type RemoveFirstFromTuple<T extends any[]> = ((...b: T) => any) extends (
 export type RemoveEditorOnFirstParamIfNeed<F extends (...args: any[]) => any, R> = (
   ...args: Parameters<F>[0] extends Editor ? RemoveFirstFromTuple<Parameters<F>> : Parameters<F>
 ) => R;
+
+/**
+ * @example
+ *
+ * type BlockRenderer = PickPluginAndRequired<'renderBlock'> === {
+ *  renderBlock: (props: RenderBlockProps, editor: CoreEditor, next: () => any) => any;
+ * }
+ */
+export type PickPluginAndRequired<K extends keyof Plugin> = Pick<Required<Plugin>, K>;
