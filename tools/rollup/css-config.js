a0b88d9573f795b1f8407e4c0577fb161a387df0
Jay Chen <gcobs135102@gmail.com>
2019-11-08 16:40:21 +0800

    chore: add autoprefixer

--- tools/rollup/css-config.js
+++ tools/rollup/css-config.js
@@ -1,10 +1,12 @@
 import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
 
 export const plugins = [
   postcss({
     extract: false,
     modules: false,
     minimize: true,
-    extensions: ['scss']
+    extensions: ['scss'],
+    plugins: [autoprefixer()]
   })
 ];
