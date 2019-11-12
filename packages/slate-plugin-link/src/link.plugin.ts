9094d4ab84efc684ef6a4ac0e4c9a7a3e3944bae
Jay Chen <gcobs135102@gmail.com>
2019-11-12 15:20:53 +0800

    chore(package): upgrade typescript eslint to v2.7

--- packages/slate-plugin-link/src/link.plugin.ts
+++ packages/slate-plugin-link/src/link.plugin.ts
@@ -16,15 +16,7 @@ export interface LinkPlugin extends PickPluginAndRequired<'renderInline' | 'rend
 }
 
 export function LinkPlugin(config?: LinkPluginConfig): LinkPlugin {
-  /**
-   * @todo
-   * remove the eslint disable.
-   * @see
-   * https://github.com/typescript-eslint/typescript-eslint/issues/1104
-   */
-  //  eslint-disable-next-line @typescript-eslint/no-use-before-define
   const type = config?.type ?? LINK_TYPE;
-  //  eslint-disable-next-line @typescript-eslint/no-use-before-define
   const renderLinkModal = config?.renderLinkModal;
   const queries = LinkQueries(type);
   const commands = LinkCommands({ type, queryHas: queries[LINK_QUERY_HAS] });
--- packages/slate-plugin-toggle-mark/src/toggle-mark.plugin.ts
+++ packages/slate-plugin-toggle-mark/src/toggle-mark.plugin.ts
@@ -23,15 +23,7 @@ export interface ToggleMarkPlugin extends PickPluginAndRequired<'onKeyDown' | 'r
 
 export function createToggleMarkPlugin(defaults: ToggleMarkPluginDefaultConfig) {
   function ToggleMarkPlugin(config?: ToggleMarkPluginConfig): ToggleMarkPlugin {
-    /**
-     * @todo
-     * remove the eslint disable.
-     * @see
-     * https://github.com/typescript-eslint/typescript-eslint/issues/1104
-     */
-    //  eslint-disable-next-line @typescript-eslint/no-use-before-define
     const type = config?.type ?? defaults.type;
-    //  eslint-disable-next-line @typescript-eslint/no-use-before-define
     const hotkey = config?.hotkey ?? defaults.hotkey;
     const { component, queryHas, commandAdd, commandRemove, commandToggle } = defaults;
 
