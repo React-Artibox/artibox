0c30823f6bfaa5c5b5c641bf49ce161c7e578791
Jay Chen <gcobs135102@gmail.com>
2019-11-13 12:49:29 +0800

    feat(icons): add list icons

---
+++ packages/icons/src/icons/unordered-list.ts
@@ -0,0 +1,17 @@
+import { IconDefinition } from '../types';
+
+export const UnorderedList: IconDefinition = {
+  name: 'unordered list',
+  definition: {
+    svg: {
+      viewBox: '0 0 24 24'
+    },
+    path: {
+      fillRule: 'evenodd',
+      fill: 'currentColor',
+      strokeWidth: 0,
+      d:
+        'M3.5 16.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 1 1 0-3zM22 17v2H7v-2h15zM3.5 10.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 1 1 0-3zM22 11v2H7v-2h15zM3.5 4.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 1 1 0-3zM22 5v2H7V5h15z'
+    }
+  }
+};
