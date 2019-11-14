0c77c0edb3909110f0e6589469f821348c124d5a
Jay Chen <gcobs135102@gmail.com>
2019-11-14 14:57:22 +0800

    docs(slate-core): add comments on isNodeExcludeText

--- packages/slate-core/src/utils/is-node-exclude-text.ts
+++ packages/slate-core/src/utils/is-node-exclude-text.ts
@@ -1,5 +1,8 @@
 import { Node, Document, Block, Inline } from 'slate';
 
+/**
+ * Check if a node is either `Document` or `Block` or `Inline`.
+ */
 export function isNodeExcludeText(node?: Node | null): node is Document | Block | Inline {
   if (!node) {
     return false;
