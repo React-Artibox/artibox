a15c320678fb418cb694e2b7fa9ca36166e4e1d9
Jay Chen <gcobs135102@gmail.com>
2019-11-14 16:24:09 +0800

    chore: lint all files
    Add format script.
    Add css, sass, scss, json, md to lint-staged.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  '*.@(js|jsx|ts|tsx)': ['prettier --write', 'yarn lint --fix', 'git add']
+  '*.@(js|jsx|ts|tsx)': ['prettier --write', 'yarn lint:ts --fix', 'git add'],
+  '*.@(css|sass|scss)': ['prettier --write', 'yarn lint:css --fix', 'git add'],
+  '*.@(json|md)': ['prettier --write', 'git add']
 };
