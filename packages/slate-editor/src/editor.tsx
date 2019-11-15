f8e52b651122cf4d68025472423bf3a487a82911
Jay Chen <gcobs135102@gmail.com>
2019-11-15 10:59:08 +0800

    refactor: move default theme to components and clear deps about theme

--- packages/slate-editor/src/editor.tsx
+++ packages/slate-editor/src/editor.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Editor, EditorProps } from 'slate-react';
 import cx from 'classnames';
 import { ThemeProvider } from '@artibox/components';
-import '@artibox/theme-default';
 
 export interface ArtiboxEditorProps extends EditorProps {
   theme?: string;
