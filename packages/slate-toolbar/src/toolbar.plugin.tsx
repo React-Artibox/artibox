869044466db8b6c0e1798941c81df9d57c47d097
Jay Chen <gcobs135102@gmail.com>
2019-11-19 12:04:02 +0800

    feat(slate-toolbar): add disabledBlocks config

--- packages/slate-toolbar/src/toolbar.plugin.tsx
+++ packages/slate-toolbar/src/toolbar.plugin.tsx
@@ -7,19 +7,26 @@ import './styles';
 export interface ToolbarPluginConfig {
   collapsedTools?: Tool[];
   expandedTools?: Tool[];
+  disabledBlocks?: string[];
 }
 
 export type ToolbarPlugin = CommonEditorRenderer;
 
 export function ToolbarPlugin(config: ToolbarPluginConfig): ToolbarPlugin {
-  const { collapsedTools, expandedTools } = config;
+  const { collapsedTools, expandedTools, disabledBlocks } = config;
 
   return CommonEditorRenderer({
-    render: (editor, el) => (
-      <>
-        {el}
-        <Toolbar collapsedTools={collapsedTools} expandedTools={expandedTools} editor={editor} />
-      </>
-    )
+    render: (editor, el) => {
+      if (disabledBlocks && editor.value.blocks.some(block => disabledBlocks.includes(block!.type))) {
+        return el;
+      }
+
+      return (
+        <>
+          {el}
+          <Toolbar collapsedTools={collapsedTools} expandedTools={expandedTools} editor={editor} />
+        </>
+      );
+    }
   });
 }
