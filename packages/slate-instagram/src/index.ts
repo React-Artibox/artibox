b1c6a911f9bffd773b6d57e9afe3a2f866f7fdc7
Jay Chen <gcobs135102@gmail.com>
2019-11-20 17:46:05 +0800

    refactor(slate-instagram): moved from slate-plugin-instagram
    Extract and merge queries and commands to utils.

---
+++ packages/slate-instagram/src/index.ts
@@ -0,0 +1,3 @@
+export { INSTAGRAM_TYPE, INSTAGRAM_DATA_KEY_URL, INSTAGRAM_REG } from './instagram.constants';
+export { InstagramUtils } from './instagram.utils';
+export { InstagramConfig, Instagram } from './instagram';
