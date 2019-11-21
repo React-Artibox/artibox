fc496d7a639c47157df8089ca2a59f1527264d26
Jay Chen <gcobs135102@gmail.com>
2019-11-21 12:36:17 +0800

    refactor(slate-instagram): move utils to root class

--- packages/slate-instagram/src/instagram.utils.ts
+++ packages/slate-instagram/src/instagram.utils.ts
@@ -1,56 +1,24 @@
-import { Block, Editor } from 'slate';
-import { PARAGRAPH_TYPE } from '@artibox/slate-core';
-import { INSTAGRAM_DATA_KEY_URL } from './instagram.constants';
+export function getInstagramUrlFromEmbedCode(embedCode: string): string | undefined {
+  const template = document.createElement('template');
+  template.innerHTML = embedCode;
+  const { firstChild } = template.content;
 
-export interface InstagramUtils {
-  getInstagramUrlFromEmbedCode(embedCode: string): string | undefined;
-  createInstagramBlock(url: string): Block;
-  addInstagramBlock(editor: Editor, embedCode: string): Editor;
-}
+  if (!firstChild || firstChild.nodeName !== 'BLOCKQUOTE') {
+    return undefined;
+  }
 
-export function InstagramUtils(type: string): InstagramUtils {
-  const getInstagramUrlFromEmbedCode: InstagramUtils['getInstagramUrlFromEmbedCode'] = embedCode => {
-    const template = document.createElement('template');
-    template.innerHTML = embedCode;
-    const { firstChild } = template.content;
+  const blockquote = firstChild as HTMLElement;
+  const link = (blockquote.firstChild as HTMLDivElement).children[0] as HTMLLinkElement | null | undefined;
 
-    if (!firstChild || firstChild.nodeName !== 'BLOCKQUOTE') {
-      return undefined;
-    }
+  if (!link) {
+    return undefined;
+  }
 
-    const blockquote = firstChild as HTMLElement;
-    const link = (blockquote.firstChild as HTMLDivElement).children[0] as HTMLLinkElement | null | undefined;
+  const href = link.getAttribute('href');
 
-    if (!link) {
-      return undefined;
-    }
+  if (!href) {
+    return undefined;
+  }
 
-    const href = link.getAttribute('href');
-
-    if (!href) {
-      return undefined;
-    }
-
-    return href.replace(/^https:\/\/www\.instagram\.com\//, '').replace(/\/$/, '');
-  };
-  const createInstagramBlock: InstagramUtils['createInstagramBlock'] = url =>
-    Block.fromJSON({
-      type,
-      data: { [INSTAGRAM_DATA_KEY_URL]: url }
-    });
-  const addInstagramBlock: InstagramUtils['addInstagramBlock'] = (editor, embedCode) => {
-    const url = getInstagramUrlFromEmbedCode(embedCode);
-
-    if (!url) {
-      return editor;
-    }
-
-    return editor.insertBlock(createInstagramBlock(url)).insertBlock(PARAGRAPH_TYPE);
-  };
-
-  return {
-    getInstagramUrlFromEmbedCode,
-    createInstagramBlock,
-    addInstagramBlock
-  };
+  return href.replace(/^https:\/\/www\.instagram\.com\//, '').replace(/\/$/, '');
 }
