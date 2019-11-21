ef11d4a9c79ab20d9dc51ff7fbc23a4415b65506
Jay Chen <gcobs135102@gmail.com>
2019-11-21 18:24:53 +0800

    refactor(slate-input-block): moved from slate-plugin-input-block
    Extract and merge queries and commands into root class.

--- packages/slate-plugin-input-block/src/input-block.types.ts
+++
@@ -1,5 +0,0 @@
-import { Editor } from 'slate';
-
-export interface InputBlockData {
-  onConfirm: (editor: Editor, value: string) => Editor;
-}
--- packages/slate-toolbar/src/toolbar.types.ts
+++ packages/slate-toolbar/src/toolbar.types.ts
@@ -1,12 +1,14 @@
 import { Editor } from 'slate';
 import { MouseEvent } from 'react';
 import { IconDefinition } from '@artibox/icons';
-import { InputBlockData } from '@artibox/slate-plugin-input-block';
 import { TOOLBAR_DIVIDER } from './toolbar.constants';
 
 export type ToolOnMouseDown = (editor: Editor, event: MouseEvent) => void;
 export type ToolIsActive = (editor: Editor) => boolean;
-export type ToolInputable = InputBlockData;
+export type ToolInputable = {
+  onStart?: (editor: Editor, data: ToolInputable) => void;
+  onConfirm(editor: Editor, value: string): Editor;
+};
 
 export type ToolConfig = {
   onMouseDown?: ToolOnMouseDown;
