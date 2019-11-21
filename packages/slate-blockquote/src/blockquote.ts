5f42f04c91bc92c56860d88226e8543db4b2fdd5
Jay Chen <gcobs135102@gmail.com>
2019-11-21 12:27:21 +0800

    refactor(slate-blockquote): move utils to root class

--- packages/slate-blockquote/src/blockquote.ts
+++ packages/slate-blockquote/src/blockquote.ts
@@ -1,7 +1,8 @@
+import { Block } from 'slate';
 import { CommonBlockRenderer, RendererBaseComponent } from '@artibox/slate-renderer';
-import { BlockquoteUtils } from './blockquote.utils';
-import { BlockquoteHandlers } from './blockquote.handlers';
 import { BLOCKQUOTE_TYPE, BLOCKQUOTE_HOTKEY, BLOCKQUOTE_COMPONENT } from './blockquote.constants';
+import { BlockquoteHandlers } from './blockquote.handlers';
+import { BlockquoteController } from './blockquote.interfaces';
 
 export interface BlockquoteConfig {
   type: string;
@@ -9,25 +10,57 @@ export interface BlockquoteConfig {
   component: RendererBaseComponent;
 }
 
-export class Blockquote {
+export class Blockquote implements BlockquoteController {
+  static Handlers = BlockquoteHandlers;
+  static Renderer = CommonBlockRenderer;
+
   static create(config?: BlockquoteConfig) {
     const type = config?.type ?? BLOCKQUOTE_TYPE;
     const hotkey = config?.hotkey ?? BLOCKQUOTE_HOTKEY;
     const component = config?.component ?? BLOCKQUOTE_COMPONENT;
-    const utils = BlockquoteUtils(type);
-    const handlers = BlockquoteHandlers(hotkey, utils);
-    const renderer = CommonBlockRenderer({ type, component });
 
-    return new this(utils, handlers, renderer);
+    return new this(type, blockquote => this.Handlers(hotkey, blockquote), this.Renderer({ type, component }));
   }
 
+  plugin = {
+    ...this.handlersFactory(this),
+    ...this.renderer
+  } as const;
+
   constructor(
-    public readonly utils: BlockquoteUtils,
-    private readonly handlers: BlockquoteHandlers,
+    public readonly type: string,
+    private readonly handlersFactory: (blockquoteController: BlockquoteController) => BlockquoteHandlers,
     private readonly renderer: CommonBlockRenderer
   ) {}
 
-  get plugin() {
-    return { ...this.handlers, ...this.renderer };
-  }
+  isBlockAsBlockquote: BlockquoteController['isBlockAsBlockquote'] = block => {
+    if (!block) {
+      return false;
+    }
+
+    return block.type === this.type;
+  };
+
+  isSelectionInBlockquote: BlockquoteController['isSelectionInBlockquote'] = editor =>
+    !!this.getCurrentBlockquote(editor);
+
+  getCurrentBlockquote: BlockquoteController['getCurrentBlockquote'] = editor => {
+    const block = editor.value.startBlock as Block | null;
+
+    if (!block) {
+      return null;
+    } else if (this.isBlockAsBlockquote(block)) {
+      return block;
+    }
+
+    const parent = editor.value.document.getParent(block.key) as Block | null;
+    return this.isBlockAsBlockquote(parent) ? parent : null;
+  };
+
+  wrapBlockquote: BlockquoteController['wrapBlockquote'] = editor => editor.wrapBlock(this.type);
+
+  unwrapBlockquote: BlockquoteController['unwrapBlockquote'] = editor => editor.unwrapBlock(this.type);
+
+  toggleBlockquote: BlockquoteController['toggleBlockquote'] = editor =>
+    this.isSelectionInBlockquote(editor) ? this.unwrapBlockquote(editor) : this.wrapBlockquote(editor);
 }
