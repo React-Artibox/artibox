32ce98b112e662136fe5aee4435693214c64734b
Jay Chen <gcobs135102@gmail.com>
2019-11-29 17:51:59 +0800

    fix(slate-toolbar): focus issue
    Won't lost the selection range after input blurred.

--- packages/slate-toolbar/src/components/toolbar-input.tsx
+++ packages/slate-toolbar/src/components/toolbar-input.tsx
@@ -1,4 +1,4 @@
-import React, { KeyboardEventHandler } from 'react';
+import React, { KeyboardEventHandler, KeyboardEvent, MouseEvent } from 'react';
 import { WithEditor, InputData, SetInputData } from '@artibox/slate-common';
 import { useLocale } from '@artibox/components/locale';
 import { clsPrefix } from './constants';
@@ -14,7 +14,8 @@ function ToolbarInput({ editor, toolInput, setToolInput }: ToolbarInputProps) {
       input.focus();
     }
   };
-  const exitInputAndFocus = () => {
+  const onExitInputAndFocus = (event: KeyboardEvent | MouseEvent) => {
+    event.preventDefault();
     setToolInput(null);
     editor.focus();
   };
@@ -26,14 +27,13 @@ function ToolbarInput({ editor, toolInput, setToolInput }: ToolbarInputProps) {
     const { onConfirm } = toolInput;
 
     if (event.key === 'Enter') {
-      event.preventDefault();
       onConfirm(editor, (event.target as HTMLInputElement).value);
-      exitInputAndFocus();
+      onExitInputAndFocus(event);
     } else if (event.key === 'Escape') {
-      event.preventDefault();
-      exitInputAndFocus();
+      onExitInputAndFocus(event);
     }
   };
+  const onBlur = () => setToolInput(null);
   const { getPlaceholder } = toolInput;
   const locale = useLocale();
   const placeholder = getPlaceholder(locale);
@@ -45,9 +45,9 @@ function ToolbarInput({ editor, toolInput, setToolInput }: ToolbarInputProps) {
         className={`${clsPrefix}__input`}
         placeholder={placeholder}
         onKeyDown={onKeyDown}
-        onBlur={exitInputAndFocus}
+        onBlur={onBlur}
       />
-      <svg className={`${clsPrefix}__input__cross`} viewBox="0 0 320 512" onClick={exitInputAndFocus}>
+      <svg className={`${clsPrefix}__input__cross`} viewBox="0 0 320 512" onMouseDown={onExitInputAndFocus}>
         <path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z" />
       </svg>
     </div>
