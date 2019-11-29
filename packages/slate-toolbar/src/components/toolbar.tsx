51e22fd4f87c8ce28c41dee24f463b0c0286d262
Jay Chen <gcobs135102@gmail.com>
2019-11-29 18:39:23 +0800

    feat(slate-toolbar): make toolbar as tooltip style
    And let position of collapsed toolbar follow caret.

--- packages/slate-toolbar/src/components/toolbar.tsx
+++ packages/slate-toolbar/src/components/toolbar.tsx
@@ -1,5 +1,4 @@
 import React, { useRef, useLayoutEffect, useState } from 'react';
-import { Editor } from 'slate-react';
 import cx from 'classnames';
 import { WithEditor, InputData } from '@artibox/slate-common';
 import { useTheme } from '@artibox/components/theme';
@@ -21,23 +20,16 @@ function roundNumber(value: number, min: number, max: number) {
   return value;
 }
 
-function calculatePosition(el: HTMLElement, editor: Editor, expanded?: boolean) {
+function calculatePosition(el: HTMLElement) {
   const native = window.getSelection();
   const range = native!.getRangeAt(0);
   const rect = range.getBoundingClientRect();
   const top = rect.top + window.pageYOffset - el.offsetHeight;
-  let left: number;
-
-  if (expanded) {
-    left = roundNumber(
-      rect.left + window.pageXOffset - (el.offsetWidth - rect.width) / 2,
-      0,
-      window.innerWidth - el.offsetWidth
-    );
-  } else {
-    const editorEl: HTMLElement = (editor as any).el;
-    left = editorEl.getBoundingClientRect().right - el.offsetWidth;
-  }
+  const left = roundNumber(
+    rect.left + window.pageXOffset - (el.offsetWidth - rect.width) / 2,
+    0,
+    window.innerWidth - el.offsetWidth
+  );
 
   return { top, left };
 }
@@ -76,7 +68,7 @@ function Toolbar({ collapsedTools, expandedTools, editor }: ToolbarProps) {
         return;
       }
 
-      const { top, left } = calculatePosition(el, (editor as any) as Editor, expanded);
+      const { top, left } = calculatePosition(el);
 
       el.style.top = `${top}px`;
       el.style.left = `${left}px`;
@@ -95,17 +87,20 @@ function Toolbar({ collapsedTools, expandedTools, editor }: ToolbarProps) {
 
   return (
     <Portal>
-      <div ref={ref} className={cx(clsPrefix, theme)}>
-        {tools.map((tool, index) => {
-          if (tool === TOOLBAR_DIVIDER) {
-            return <Divider key={index} />;
-          }
+      <div ref={ref} className={cx(`${clsPrefix}__wrapper`, theme)}>
+        <div className={`${clsPrefix}__arrow`} />
+        <div className={clsPrefix}>
+          {tools.map((tool, index) => {
+            if (tool === TOOLBAR_DIVIDER) {
+              return <Divider key={index} />;
+            }
 
-          const { icon, hook } = tool;
+            const { icon, hook } = tool;
 
-          return <ToolbarIcon key={icon.name} icon={icon} hook={hook} editor={editor} setToolInput={setToolInput} />;
-        })}
-        {expanded && toolInput && <ToolbarInput editor={editor} toolInput={toolInput} setToolInput={setToolInput} />}
+            return <ToolbarIcon key={icon.name} icon={icon} hook={hook} editor={editor} setToolInput={setToolInput} />;
+          })}
+          {expanded && toolInput && <ToolbarInput editor={editor} toolInput={toolInput} setToolInput={setToolInput} />}
+        </div>
       </div>
     </Portal>
   );
