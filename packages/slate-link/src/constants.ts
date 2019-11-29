f6657315899ecb2a40d3ebd3726919ae4955134e
Jay Chen <gcobs135102@gmail.com>
2019-11-29 16:36:33 +0800

    feat(slate-link): add tooltip to link component

--- packages/slate-link/src/constants.ts
+++ packages/slate-link/src/constants.ts
@@ -1,5 +1,2 @@
 export const LINK_TYPE = 'link' as const;
 export type LINK_TYPE = typeof LINK_TYPE;
-
-export const LINK_COMPONENT = 'a' as const;
-export type LINK_COMPONENT = typeof LINK_COMPONENT;
--- packages/slate-link/src/index.ts
+++ packages/slate-link/src/index.ts
@@ -1,4 +1,4 @@
-export { LINK_TYPE, LINK_COMPONENT } from './constants';
+export { LINK_TYPE } from './constants';
 export { LinkProps } from './types';
 export { LinkForPluginConfig, LinkForToolHookConfig, Link, CreateLinkConfig, createLink } from './link';
 export { CreateLinkJsxSerializerRuleConfig, createLinkJsxSerializerRule } from './jsx-serializer';
