ae1bdbd06a324f509dff3754e5e5c7841b4bc1b0
Jay Chen <gcobs135102@gmail.com>
2019-12-04 15:03:37 +0800

    docs(slate-separation-line): add comments and README

--- packages/slate-separation-line/src/constants.ts
+++ packages/slate-separation-line/src/constants.ts
@@ -1,5 +1,11 @@
-export const SEPARATION_LINE_TYPE = 'separation line';
+/**
+ * Default type of separation line.
+ */
+export const SEPARATION_LINE_TYPE = 'separation_line';
 export type SEPARATION_LINE_TYPE = typeof SEPARATION_LINE_TYPE;
 
+/**
+ * Default component of both renderer of editor and jsx serializer rule of separation line.
+ */
 export const SEPARATION_LINE_COMPONENT = 'hr';
 export type SEPARATION_LINE_COMPONENT = typeof SEPARATION_LINE_COMPONENT;
