c81d1a6f40a299163fa22dcb9923e1052bfb6e8c
Jay Chen <gcobs135102@gmail.com>
2019-12-05 16:53:34 +0800

    docs(components): add comments and README

--- packages/components/src/utils/compose-refs.ts
+++ packages/components/src/utils/compose-refs.ts
@@ -1,5 +1,22 @@
 import { Ref } from 'react';
 
+/**
+ * Compose all refs to single one.
+ * It's helpful if you want to use useRef in an forwardRef component.
+ *
+ * @example
+ *
+ * const Some = forwardRef((props, ref) => {
+ *   const refFromHook = useRef(null);
+ *   const composedRef = composeRefs([ref, refFromHook]);
+ *
+ *   return (
+ *     <div ref{composedRef}>
+ *       ...
+ *     </div>
+ *   );
+ * });
+ */
 export function composeRefs<T>(refs: Ref<T>[]) {
   return (element: T) => {
     refs.forEach(ref => {
