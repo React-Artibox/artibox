2270e97478d276ee96cf4fb1c6ac85326c2f5e1f
Jay Chen <gcobs135102@gmail.com>
2019-12-06 12:51:19 +0800

    docs: add get-started.md and root README

---
+++ docs/module-factories.md
@@ -0,0 +1,4 @@
+# Module Factories
+
+- [forPlugin](../packages/slate-common/src/typings/common.ts#L16)
+- [forToolHook](../packages/slate-common/src/typings/tool.ts#L18)
--- stories/rich-editor/plugins.ts
+++ stories/rich-editor/plugins.ts
@@ -1,22 +1,22 @@
 import { Plugin } from 'slate-react';
 import {
-  Bold,
-  Italic,
-  Underline,
-  Strikethrough,
-  Highlight,
-  Link,
+  Bold as BoldIcon,
+  Italic as ItalicIcon,
+  Underline as UnderlineIcon,
+  Strikethrough as StrikethroughIcon,
+  Highlight as HighlightIcon,
+  Link as LinkIcon,
   Unlink,
   Heading1,
   Heading2,
   Heading3,
-  Blockquote,
+  Blockquote as BlockquoteIcon,
   UnorderedList,
   OrderedList,
-  SeparationLine,
-  Video,
-  Facebook,
-  Instagram
+  SeparationLine as SeparationLineIcon,
+  Video as VideoIcon,
+  Facebook as FacebookIcon,
+  Instagram as InstagramIcon
 } from '@artibox/icons';
 import { createBold } from '@artibox/slate-bold';
 import { createItalic } from '@artibox/slate-italic';
@@ -34,59 +34,59 @@ import { createInstagram } from '@artibox/slate-instagram';
 import { createInputBlock } from '@artibox/slate-input-block';
 import { Toolbar, TOOLBAR_DIVIDER } from '@artibox/slate-toolbar';
 
-const bold = createBold();
-const italic = createItalic();
-const underline = createUnderline();
-const strikethrough = createStrikethrough();
-const highlight = createHighlight();
-const link = createLink();
-const heading = createHeading();
-const blockquote = createBlockquote();
-const list = createList();
-const separationLine = createSeparationLine();
-const video = createVideo();
-const instagram = createInstagram();
-const facebook = createFacebook();
-const inputBlock = createInputBlock();
+const Bold = createBold();
+const Italic = createItalic();
+const Underline = createUnderline();
+const Strikethrough = createStrikethrough();
+const Highlight = createHighlight();
+const Link = createLink();
+const Heading = createHeading();
+const Blockquote = createBlockquote();
+const List = createList();
+const SeparationLine = createSeparationLine();
+const Video = createVideo();
+const Instagram = createInstagram();
+const Facebook = createFacebook();
+const InputBlock = createInputBlock();
 
 export const plugins: Plugin[] = [
-  bold.forPlugin(),
-  italic.forPlugin(),
-  underline.forPlugin(),
-  strikethrough.forPlugin(),
-  highlight.forPlugin(),
-  link.forPlugin(),
-  heading.forPlugin({ disabled: [4, 5, 6] }),
-  blockquote.forPlugin(),
-  ...list.forPlugin(),
-  separationLine.forPlugin(),
-  video.forPlugin(),
-  instagram.forPlugin(),
-  facebook.forPlugin(),
-  inputBlock.forPlugin(),
+  Bold.forPlugin(),
+  Italic.forPlugin(),
+  Underline.forPlugin(),
+  Strikethrough.forPlugin(),
+  Highlight.forPlugin(),
+  Link.forPlugin(),
+  Heading.forPlugin({ disabled: [4, 5, 6] }),
+  Blockquote.forPlugin(),
+  ...List.forPlugin(),
+  SeparationLine.forPlugin(),
+  Video.forPlugin(),
+  Instagram.forPlugin(),
+  Facebook.forPlugin(),
+  InputBlock.forPlugin(),
   Toolbar.forPlugin({
-    disabledBlocks: [inputBlock.type],
+    disabledBlocks: [InputBlock.type],
     expandedTools: [
-      { icon: Bold, hook: bold.forToolHook() },
-      { icon: Italic, hook: italic.forToolHook() },
-      { icon: Underline, hook: underline.forToolHook() },
-      { icon: Strikethrough, hook: strikethrough.forToolHook() },
-      { icon: Highlight, hook: highlight.forToolHook() },
-      { icon: Link, hook: link.forToolHook() },
-      { icon: Unlink, hook: link.forToolHook({ command: 'remove' }) }
+      { icon: BoldIcon, hook: Bold.forToolHook() },
+      { icon: ItalicIcon, hook: Italic.forToolHook() },
+      { icon: UnderlineIcon, hook: Underline.forToolHook() },
+      { icon: StrikethroughIcon, hook: Strikethrough.forToolHook() },
+      { icon: HighlightIcon, hook: Highlight.forToolHook() },
+      { icon: LinkIcon, hook: Link.forToolHook() },
+      { icon: Unlink, hook: Link.forToolHook({ command: 'remove' }) }
     ],
     collapsedTools: [
-      { icon: Heading1, hook: heading.forToolHook({ level: 1 }) },
-      { icon: Heading2, hook: heading.forToolHook({ level: 2 }) },
-      { icon: Heading3, hook: heading.forToolHook({ level: 3 }) },
-      { icon: Blockquote, hook: blockquote.forToolHook() },
-      { icon: UnorderedList, hook: list.forToolHook({ orderedType: 'unordered' }) },
-      { icon: OrderedList, hook: list.forToolHook({ orderedType: 'ordered' }) },
+      { icon: Heading1, hook: Heading.forToolHook({ level: 1 }) },
+      { icon: Heading2, hook: Heading.forToolHook({ level: 2 }) },
+      { icon: Heading3, hook: Heading.forToolHook({ level: 3 }) },
+      { icon: BlockquoteIcon, hook: Blockquote.forToolHook() },
+      { icon: UnorderedList, hook: List.forToolHook({ orderedType: 'unordered' }) },
+      { icon: OrderedList, hook: List.forToolHook({ orderedType: 'ordered' }) },
       TOOLBAR_DIVIDER,
-      { icon: SeparationLine, hook: separationLine.forToolHook() },
-      { icon: Video, hook: video.forToolHook({ setInputConfig: inputBlock.start }) },
-      { icon: Instagram, hook: instagram.forToolHook({ setInputConfig: inputBlock.start }) },
-      { icon: Facebook, hook: facebook.forToolHook({ setInputConfig: inputBlock.start }) }
+      { icon: SeparationLineIcon, hook: SeparationLine.forToolHook() },
+      { icon: VideoIcon, hook: Video.forToolHook({ setInputConfig: InputBlock.start }) },
+      { icon: InstagramIcon, hook: Instagram.forToolHook({ setInputConfig: InputBlock.start }) },
+      { icon: FacebookIcon, hook: Facebook.forToolHook({ setInputConfig: InputBlock.start }) }
     ]
   })
 ];
