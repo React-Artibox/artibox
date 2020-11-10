c9b2dea586a83031ee25dbb3ae78031dbb762c16
Jay Chen <gcobs135102@gmail.com>
2020-11-10 14:57:37 +0800

    feat(icons): add read-more and twitter icons

---
+++ packages/icons/src/read-more.ts
@@ -0,0 +1,15 @@
+import { IconDefinition } from './typings';
+
+export const ReadMore: IconDefinition = {
+  name: 'read-more',
+  definition: {
+    svg: {
+      viewBox: '0 0 24 24'
+    },
+    path: {
+      fill: 'currentColor',
+      d:
+        'M22,16 L22,19 L2,19 L2,16 L22,16 Z M7,11 L7,13 L2,13 L2,11 L7,11 Z M15,11 L15,13 L9,13 L9,11 L15,11 Z M22,11 L22,13 L17,13 L17,11 L22,11 Z M22,5 L22,8 L2,8 L2,5 L22,5 Z'
+    }
+  }
+};
