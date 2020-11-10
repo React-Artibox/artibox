e108d04da42db98b3a34a2b67dbdda4556c560bf
Jay Chen <gcobs135102@gmail.com>
2020-11-10 14:56:13 +0800

    feat(utils): add convertCamelToKebab and pipe

---
+++ packages/utils/src/convertCamelToKebab.ts
@@ -0,0 +1,6 @@
+export function convertCamelToKebab(str: string) {
+  return str
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z])(?=[a-z])/g, '$1-$2')
+    .toLowerCase();
+}
