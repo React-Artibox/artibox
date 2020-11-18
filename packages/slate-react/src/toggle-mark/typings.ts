0c3a4cfbccdae8a5f99222e497e574438980dd24
Jay Chen <gcobs135102@gmail.com>
2020-11-18 13:25:26 +0800

    docs: add props to docgens of toggle-mark

--- packages/slate-react/src/toggle-mark/typings.ts
+++ packages/slate-react/src/toggle-mark/typings.ts
@@ -2,7 +2,6 @@ import { ToggleMark } from '@artibox/slate-common/toggle-mark';
 import { CreateRenderMarkOptions, RenderMarkProps, WithCreateHandlers, WithCreateRenderLeaf } from '../core';
 
 export type RenderToggleMarkProps = RenderMarkProps<boolean>;
-export type RenderToggleMark = (props: RenderToggleMarkProps) => JSX.Element;
 
 export interface ReactToggleMarkCreateHandlersOptions {
   /**
--- stories/marks/highlight/docgen.ts
+++ stories/marks/highlight/docgen.ts
@@ -1,9 +1,13 @@
 import {
   ReactToggleMark,
   ReactToggleMarkCreateHandlersOptions,
-  ReactToggleMarkCreateRenderLeafOptions
+  ReactToggleMarkCreateRenderLeafOptions,
+  RenderToggleMarkProps
 } from '@artibox/slate-react/toggle-mark';
+import { JsxSerializeToggleMarkProps } from '@artibox/slate-react/toggle-mark/jsx-serializer';
 
-export const ReactToggleMarkDocgen = (t: ReactToggleMark) => t;
 export const createHandlersDocgen = (options: ReactToggleMarkCreateHandlersOptions) => options;
 export const createRenderLeafDocgen = (options: ReactToggleMarkCreateRenderLeafOptions) => options;
+export const ReactToggleMarkDocgen = (t: ReactToggleMark) => t;
+export const RenderToggleMarkPropsDocgen = (props: RenderToggleMarkProps) => props;
+export const JsxSerializeToggleMarkPropsDocgen = (props: JsxSerializeToggleMarkProps) => props;
